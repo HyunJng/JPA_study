fbca1fb73009a0d4d9f7df825d79d6084bf5dd68
HyunJng <khjung1654@gmail.com>
2023-05-02 17:47:27 +0900

    실전!스프링부트와JPA활용2 - API 개발 기본

---
+++ jpashop/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,106 @@
+package jpabook.jpashop.api;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberResponse {
+        private Long id;
+
+        public CreateMemberResponse(Long id) {
+            this.id = id;
+        }
+    }
+
+    /**
+     * 2. DTO를 사용해서 Request받는다.
+     * - Entity는 많은 곳에서 사용하는 것이기 때문에 직접 사용해서
+     *  누가 API Spec을 바꾸게 되면 에러가 생김
+     *  API spec이 바뀌지 않는다는 장점
+     * - Entity만 보고는 어떤 것이 들어올지 모르는데 DTO를 사용하면
+     *  필요한 것만 받을 수 있다.
+     * - Validation도 DTO에 넣을 수 있어 서비스마다 Validation을 fit하게
+     *  맞출 수 있다.
+     * */
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
+        Member member = new Member();
+        member.setName(request.name);
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class CreateMemberRequest {
+        private String name;
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable Long id
+            , @RequestBody @Valid UpdateMemberRequest request) { // 등록이랑 수정은 별도의 Request Response Dto를 사용하는 것이 좋다.
+        memberService.update(id, request.getName());
+        Member member = memberService.findOne(id);
+        return new UpdateMemberResponse(member.getId(), member.getName());
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse{
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
+    }
+
+    @GetMapping("/api/v1/members")
+    public List<Member> memberV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> members = memberService.findMembers();
+        List<MemberDto> collect = members.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect.size(), collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private int count;
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
+}
--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.domain;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,12 +18,14 @@ public class Member {
     @Column(name="member_id") // 실제 테이블에서는 컬럼 명을 member_id로 생성하기 위함
     private Long id;
 
+    @NotEmpty
     private String name;
     
     @Embedded // 내장타입이다
     private Address address;
 
     // order의 member필드에 있는 것을 반영하는 거울일 뿐이야
+//    @JsonIgnore
     @OneToMany(mappedBy = "member") // Member이 One쪽이기 때문, 주인이 아니라는 의미
     private List<Order> orders = new ArrayList<>();
 
