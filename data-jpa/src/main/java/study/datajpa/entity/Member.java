f1122e235a65047dc13f3ab42a8bbfd352cc4a57
HyunJng <khjung1654@gmail.com>
2023-06-11 21:55:03 +0900

    실전! 스프링 데이터 JPA - 예제도메인 생성 & 공통 인터페이스 기능

---
+++ data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,51 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)// Entity는 디폴트 생성자가 있어야함(프록시 때문). protected로 생성
+@ToString(of = {"id", "username", "age"})
+//@Setter// setter은 entity에 안쓰는 편이시다.
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String username;
+
+    private Integer age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, Integer age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public Member(String username, Integer age) {
+        this.username = username;
+        this.age = age;
+    }
+
+    public void changeUsername(String username) {
+        this.username = username;
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
