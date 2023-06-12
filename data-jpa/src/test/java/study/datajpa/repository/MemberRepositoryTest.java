adf2195d73ada7f07fb60aed19892b069aacb262
HyunJng <khjung1654@gmail.com>
2023-06-12 13:47:57 +0900

    실전! 스프링 데이터 JPA - 쿼리메소드작명 & @Query & 반환타입

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -20,9 +24,11 @@ class MemberRepositoryTest {
 
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    TeamRepository teamRepository;
 
     @Test
-    public void testMember() throws Exception{
+    public void testMember() throws Exception {
         Member member = new Member("memberA");
         Member saveMember = memberRepository.save(member);
         Member findMember = memberRepository.findById(saveMember.getId())
@@ -34,7 +40,7 @@ class MemberRepositoryTest {
     }
 
     @Test
-    public void basicCRUD() throws Exception{
+    public void basicCRUD() throws Exception {
         Member member1 = new Member("member1");
         Member member2 = new Member("member2");
         memberRepository.save(member1);
@@ -62,7 +68,7 @@ class MemberRepositoryTest {
     }
 
     @Test
-    public void findByUsernameAndAgeGreaterThen() throws Exception{
+    public void findByUsernameAndAgeGreaterThen() throws Exception {
         Member m1 = new Member("AAA", 10);
         Member m2 = new Member("AAA", 20);
         memberRepository.save(m1);
@@ -73,9 +79,93 @@ class MemberRepositoryTest {
         assertThat(result.get(0).getAge()).isEqualTo(20);
         assertThat(result.size()).isEqualTo(1);
     }
-    
+
     @Test
-    public void findTop3HelloBy() throws Exception{
+    public void findTop3HelloBy() throws Exception {
         List<Member> top3HelloBy = memberRepository.findTop3HelloBy();
+    }
+
+    @Test
+    public void testQuery() throws Exception {
+        // given
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        // when
+        List<Member> result = memberRepository.findUser("AAA", 10);
+
+        // then
+        assertThat(result.get(0)).isEqualTo(m1);
+    }
+
+    @Test
+    public void findUsernameList() throws Exception {
+        // given
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        // when
+        List<String> usernameList = memberRepository.findUsernameList();
+
+        // then
+        for (String s : usernameList) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void findMemberDto() throws Exception {
+        // given
+        Member m1 = new Member("AAA", 10);
+        memberRepository.save(m1);
+
+        Team team = new Team("teamA");
+        m1.changeTeam(team);
+        teamRepository.save(team);
+
+        // when
+        List<MemberDto> memberDto = memberRepository.findMemberDto();
+
+        // then
+        for (MemberDto dto : memberDto) {
+            System.out.println("dto = " + dto);
+        }
+    }
+
+    @Test
+    public void findByNames() throws Exception {
+        // given
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        // when
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+        // then
+        for (Member member : result) {
+            System.out.println("member.getUsername() = " + member.getUsername());
+        }
+    }
+
+    @Test
+    public void returnType() throws Exception {
+        // given
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> list = memberRepository.findListByUsername("AAA");
+        Member member = memberRepository.findMemberByUsername("AAA");
+        Optional<Member> optional = memberRepository.findOptionalByUsername("AAA");
+
+        assertThat(list.get(0)).isEqualTo(m1);
+        assertThat(member).isEqualTo(m1);
+        assertThat(optional.get()).isEqualTo(m1);
     }
 }
